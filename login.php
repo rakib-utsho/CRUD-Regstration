e428ebcb49c196ea3ff2a69e62c530ff18f339c0
rakib-utsho <rakibutsho1920@gmail.com>
2024-10-19 04:24:34 +0600

    form design done

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style/signUP.css">
+    <title>login page</title>
+    <style>
+        .form{
+            width: 300px;
+            height:280px;
+        }
+    </style>
+</head>
+<body>
+<div class="form">
+    <div class="title">
+        <p>Login</p>
+    </div>
+    <form action="" method="post">
+        <input type="email" name="email" placeholder="Enter Your Email">
+        <input type="password" name="password" placeholder="Enter Password">
+        <input type="submit" value="Login" name="login_button" id="login">
+    </form>
+</div>
+</body>
+</html>
---
+++ signUP.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style/signUP.css">
+    <title>Sign UP</title>
+</head>
+<body>
+<div class="form">
+    <div class="title">
+        <p>Sign UP Form</p>
+    </div>
+    <form action="" method="post">
+        <input type="text" name="name" placeholder="First Name">
+        <input type="text" name="lastName" placeholder="Last Name">
+        <input type="email" name="email" placeholder="Enter Your Email">
+        <input type="password" name="password" placeholder="Enter Password">
+        <input type="password" name="confiPassword" placeholder="Confirm Password">
+        <input type="submit" value="Sign UP" name="signUP_button" id="signUP">
+    </form>
+</div>
+</body>
+</html>
